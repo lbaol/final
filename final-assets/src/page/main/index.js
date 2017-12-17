fb2c6dc7a0120c8f3c48933543049e0cd60c0872
imbaolei <17896569@qq.com>
2017-12-18 00:10:39 +0800

    update

--- final-assets/src/page/main/index.js
+++ final-assets/src/page/main/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM,{ render } from 'react-dom';
 import SFilter from "../../components/SFilter/index.jsx";
-import SChart from "../../components/SChart/index.jsx";
+import SChart from "../../components/SChart/index2.jsx";
 import '../../common/base.scss';
 import './index.scss';
 
 render(<div className="page-wrap">
             <div className="left-nav">
-                <SFilter />
+                {/* <SFilter /> */}
             </div>
             <div className="main-content">
                 <SChart />
